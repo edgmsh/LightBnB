9e636360422f5ea17d13064af47910d6776c40e9
edgmsh <edgmsh@gmail.com>
2024-11-13 16:45:57 -0500

    LighthouseBnB INSERT

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -5,8 +5,7 @@ insert into users (
 ) values
 ('Ed', 'ed@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
 ('Mia', 'mia@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
-('Nika', 'nika@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
-('Stella', 'stella@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+('Nika', 'nika@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
 
 insert into properties (
   owner_id ,
@@ -26,7 +25,7 @@ insert into properties (
   active 
 ) values
 (1,'Speed lamp', 'description','https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350','https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 93061,6,4,8, 'Canada','536 Namsub Highway','Sotboske', 'Quebec','K2S0U7',true),
-(1,'Speed lamp', 'description','https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350','https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 85234,6,6,7, 'Canada','651 Nami Road ','Bohbatev', 'Alberta','M2M6A1',true),
+(1,'Speed line', 'description','https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350','https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 85234,6,6,7, 'Canada','651 Nami Road ','Bohbatev', 'Alberta','M2M6A1',true),
 (2,'Habit mix', 'description','https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350','https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 93061,0,5,6, 'Canada','1650 Hejto Center ','Genwezuj', 'Newfoundland And Labrador','A1Z8Y6',true);
 
 
@@ -47,7 +46,7 @@ insert into property_reviews (
   rating ,
   message  
 ) values
-(3,2,1,3,'message'),
-(2,2,2,4,'message'),
-(3,1,3,4,'message');
+(3,2,1,3,'The property was clean and well-maintained'),
+(2,2,2,4,'Great location, but a bit noisy at night.'),
+(3,1,3,4,'Enjoyed my stay, will definitely come back.');
 
