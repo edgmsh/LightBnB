daac1a072b9ea904293753ca33fad7bb2128a447
edgmsh <edgmsh@gmail.com>
2024-11-13 15:53:37 -0500

    LighthouseBnB DDL

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -26,7 +26,7 @@ CREATE TABLE properties (
   city VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
   post_code VARCHAR(255) NOT NULL,
-  active BOOLEAN NOT NULL
+  active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE reservations (
