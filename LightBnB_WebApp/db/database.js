73f090f3e5804f10b0d507ff1203822d9a6fa4e2
edgmsh <edgmsh@gmail.com>
2024-11-14 15:33:31 -0500

    function getAllReservations was converted to work with the db

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -18,7 +18,7 @@ const pool = new Pool({
  */
 const getUserWithEmail = function (email) {
   return pool
-    .query(`SELECT * FROM users where email = $1`, [email])
+    .query(`SELECT * FROM users where email = $1`, [email.toLowerCase()])
     .then((result) => {
       console.log(result.rows[0]);
       return result.rows[0];
@@ -36,11 +36,11 @@ const getUserWithId = function (id) {
   return pool
     .query(`SELECT * FROM users where id = $1`, [id])
     .then((result) => {
-      console.log(result.rows[0]);
+    //  console.log(result.rows[0]);
       return result.rows[0];
     })
     .catch((err) => {
-      console.log(err.message);
+    //  console.log(err.message);
     });
 };
 /**
@@ -59,11 +59,11 @@ const addUser = function (user) {
   return pool
   .query(query, [user.name,user.email,user.password])
   .then((result) => {
-    console.log(result.rows[0]);
+  //  console.log(result.rows[0]);
     return result.rows[0];
   })
   .catch((err) => {
-    console.log(err.message);
+   // console.log(err.message);
   });
 
 };
@@ -75,9 +75,16 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`SELECT * FROM reservations where guest_id = $1 LIMIT $2`, [guest_id,limit])
+    .then((result) => {
+     // console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+     // console.log(err.message);
+    });
 };
-
 /// Properties
 
 /**
@@ -90,11 +97,11 @@ const getAllProperties = (options, limit = 10) => {
   return pool
     .query(`SELECT * FROM properties LIMIT $1`, [limit])
     .then((result) => {
-      console.log(result.rows);
+     // console.log(result.rows);
       return result.rows;
     })
     .catch((err) => {
-      console.log(err.message);
+    //  console.log(err.message);
     });
 };
 /**
