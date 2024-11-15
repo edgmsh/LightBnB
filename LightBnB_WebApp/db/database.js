198490542814f21586b48c11c19fe6fa7dd28485
edgmsh <edgmsh@gmail.com>
2024-11-14 21:23:04 -0500

    db connection pool file refactoring

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -1,14 +1,4 @@
-const properties = require("./json/properties.json");
-const users = require("./json/users.json");
-const { Pool } = require("pg");
-
-const pool = new Pool({
-  user: "development",
-  password: "development",
-  host: "localhost",
-  database: "lightbnb",
-});
-
+const pool = require("./index.js");
 /// Users
 
 /**
---
+++ LightBnB_WebApp/db/index.js
@@ -0,0 +1,10 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "development",
+  password: "development",
+  host: "localhost",
+  database: "lightbnb",
+});
+
+module.exports = pool;
